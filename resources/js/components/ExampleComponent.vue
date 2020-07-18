c157e3a62e80b7831737b19d677f42450f14e31b
Hai <letanhai.lth@gmail.com>
2020-07-18 14:16:04 +0700

    update

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -242,6 +242,7 @@
             },
             drawLine(newLocation) {
                 if (this.data.code == null || this.data.code == this.code) {
+                    this.map.setCenter(newLocation);
                     this.map.setZoom(20);
                     this.clearMarkers();
                     this.calculateAndDisplayRoute(this.directionsService, this.directionsRenderer, 
