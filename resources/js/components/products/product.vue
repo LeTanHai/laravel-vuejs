b26a5846c21321c4bf02d9d120ccc9ff94e1e09f
Hai <letanhai.lth@gmail.com>
2020-08-16 11:27:35 +0700

    update

--- resources/js/components/products/product.vue
+++ resources/js/components/products/product.vue
@@ -219,10 +219,12 @@ export default {
             };
 
             const response = await axios.post('/export', data, {responseType: 'blob'})
-            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const url = window.URL.createObjectURL(new Blob([response.data], { 
+                type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' 
+            }));
             const link = document.createElement('a');
             link.href = url;
-            link.setAttribute('download', this.fileName); //or any other extension
+            link.setAttribute('download', 'products.xlsx'); //or any other extension
             document.body.appendChild(link);
             link.click();
 
